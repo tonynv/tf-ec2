1afe775acf66a774a450e4dc1d5b136c0a6c9469
Tony Vattathil <avattathil@gmail.com>
2020-07-01 15:46:08 -0700

    sync

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
 }
 
 
-resource "aws_instance" "t-ec2s" {
-  ami              = "ami-0edf3b95e26a682df"
-  instance_type    = "t2.micro"
-  key_name         = "tonynv"
-
+resource "aws_instance" "lunburg" {
+  ami           = "asdfssadffds"
+  instance_type = "t2.small"
 }
