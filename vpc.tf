f8ff26ca6f8b80222782854676ebf51a3e2f43d2
Tony Vattathil <avattathil@gmail.com>
2020-07-01 16:39:47 -0700

    Add azs

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,21 @@
+data "aws_availability_zones" "available" {
+}
+
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.44.0"
-  # insert the 14 required variables here
+  cidr    = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names[2]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
