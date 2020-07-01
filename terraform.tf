1c62ae35ccad97d5d82351c5f762aad9d901e8b6
Tony Vattathil <avattathil@gmail.com>
2020-07-01 16:12:18 -0700

    configure state backend

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,15 @@
-backend "remote" {
-  organization = "tonynv"
-  hostname     = "app.terraform.io"
+terraform {
+  backend "remote" {
+    organization = "tonynv"
+    hostname     = "app.terraform.io"
 
-  workspaces {
-    name = "tf-ec2"
+    workspaces {
+      name = "tf-ec2"
+    }
+  }
+
+  required_providers {
+    aws = "~> 2.25"
   }
 }
 
