684e37934a770023ec514eda145327268e386ddc
Tony Vattathil <avattathil@gmail.com>
2020-07-01 17:49:55 -0700

    update

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
   version = "2.44.0"
   cidr    = "10.0.0.0/16"
 
-  azs             = ["{data.aws_availability_zones.current_azs.names[2]}"]
+  azs             = ["{data.aws_availability_zones.current_azs.names[0]}, {data.aws_availability_zones.current_azs.names[1]}"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
